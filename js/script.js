06fb9556dfc767041f18e7fd55166e8a7d6eb827
Karolina Pastor <borek.karola@gmail.com>
2022-02-08 12:58:16 +0100

    Create and remove cookie message

--- js/script.js
+++ js/script.js
@@ -29,3 +29,36 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////
+/////////////////////////////////////
+
+const header = document.querySelector('.header');
+console.log(header);
+
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie"> Got it! </button>';
+
+header.append(message);
+
+//Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
