c11b2da465db8ade7845b5840b6fb6a1a4db5085
Karolina Pastor <borek.karola@gmail.com>
2022-02-09 12:06:00 +0100

    Add smooth scroll

--- js/script.js
+++ js/script.js
@@ -62,3 +62,21 @@ document
   .addEventListener('click', function () {
     message.remove();
   });
+
+//Smooth scroll
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  /*window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });*/
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
