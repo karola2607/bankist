c549692db907ab2ac2abbf7323730c23d5283ff1
Karolina Pastor <borek.karola@gmail.com>
2022-02-14 13:11:14 +0100

    Build a tabbed component

--- js/script.js
+++ js/script.js
@@ -55,20 +55,38 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 //////////////////////////////////////
 /////////////////////////////////////
 
 const header = document.querySelector('.header');
-console.log(header);
 
 const allSections = document.querySelectorAll('.section');
-console.log(allSections);
 
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
-
-console.log(document.getElementsByClassName('btn'));
 
 // Creating and inserting elements
 
